a5cab8945d33f8f3c4d7056566cc74021a1901c9
Tristan Burgess <tburgessdev@gmail.com>
2021-07-21 19:25:50 -0400

    lab2: ex5 LFSR calculator. all finished!

--- labs/lab2/lfsr.c
+++ labs/lab2/lfsr.c
@@ -5,6 +5,7 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t msbit = ((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 0x1;
+    *reg = (*reg >> 1) | (msbit << 15);
 }
 
